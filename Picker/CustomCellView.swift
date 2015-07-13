07b992d0c5b87809948eb18a0f035b7f5473896a
mtb.talha <talha.babar@arbisoft.com.pk>
2015-07-13 18:17:22 +0500

    Picker

---
+++ Picker/CustomCellView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomCellView.swift
+//  Picker
+//
+//  Created by Talha Babar on 7/13/15.
+//  Copyright (c) 2015 Talha Babar. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellView: UICollectionViewCell {
+    
+//    static let cellIdentifier = "CustomCellIdentifier"
+//    
+//    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var descriptionLabel: UILabel!
+//    @IBOutlet weak var productTypeLabel: UILabel!
+//    @IBOutlet weak var regionLabel: UILabel!
+//    @IBOutlet weak var progressView: UIProgressView!
+//    @IBOutlet weak var itemsRemaining: UILabel!
+//    @IBOutlet weak var itemsRemainingText: UILabel!
+//    @IBOutlet weak var totalAmount: UILabel!
+//    @IBOutlet weak var plegdedFor: UILabel!
+//    @IBOutlet weak var remainingHours: UILabel!
+//    @IBOutlet weak var hoursToGo: UILabel!
+
+
+
+}
---
+++ Picker/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by Talha Babar on 7/13/15.
+//  Copyright (c) 2015 Talha Babar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let cardLayout = UICollectionViewFlowLayout()
+//        cardLayout.sectionInset = UIEdgeInsets(top: 30, left: 20, bottom: 50, right:20)
+//        cardLayout.itemSize = CGSize(width :236,height :367)
+//        cardLayout.minimumLineSpacing = 75
+//        cardLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+//        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: cardLayout)
+//        self.collectionView.dataSource = self
+//        self.collectionView.delegate = self
+//        self.collectionView.pagingEnabled = true
+//        collectionView.registerClass(CustomCellView.self, forCellWithReuseIdentifier: CustomCellView.cellIdentifier)
+//        collectionView.backgroundColor = UIColor.whiteColor()
+//        self.collectionView.showsHorizontalScrollIndicator = false
+//        self.view.addSubview(collectionView!)
+    }
+    
+//    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+//        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(CustomCellView.cellIdentifier, forIndexPath: indexPath) as! CustomCellView
+//        cell.backgroundColor = UIColor.blackColor()
+//        cell.imageView.image = NSURL(string: <#String#>)
+//        return cell
+//    }
+
+}
+
