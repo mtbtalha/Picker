a36b3d9cf1e7cb2bdfdd82ffd4372967e22836f5
mtb.talha <talha.babar@arbisoft.com.pk>
2015-07-14 16:51:23 +0500

    Layout constraints issues are still there

--- Picker/ViewController.swift
+++ Picker/ViewController.swift
@@ -10,23 +10,17 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let cardLayout = UICollectionViewFlowLayout()
-//        cardLayout.sectionInset = UIEdgeInsets(top: 30, left: 20, bottom: 50, right:20)
-//        cardLayout.itemSize = CGSize(width :236,height :367)
-//        cardLayout.minimumLineSpacing = 75
-//        cardLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
-//        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: cardLayout)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
         self.collectionView.pagingEnabled = true
-//        collectionView.registerClass(CustomCellView.self, forCellWithReuseIdentifier: CustomCellView.cellIdentifier)
         collectionView.backgroundColor = UIColor.whiteColor()
         self.collectionView.showsHorizontalScrollIndicator = false
-        self.view.addSubview(collectionView!)
     }
     
 
@@ -36,20 +30,55 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(CustomCellView.cellIdentifier, forIndexPath: indexPath) as! CustomCellView
-//        cell.backgroundColor = UIColor.blackColor()
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(CustomCellView.cardLayoutcellIdentifier, forIndexPath: indexPath) as! CustomCellView
         let url = NSURL(string: "http://manage.allheart.com/images/detail/gg2015/gg-2015---.jpg")
-        let data = NSData(contentsOfURL: url!)
-        cell.imageView.image = UIImage(data: data!)
+       // let data = NSData(contentsOfURL: url!)
+        cell.imageView.sd_setImageWithURL(url!)
         cell.descriptionLabel.text = "Simple Shoes: Help get Quality Footwear back on your feet"
+        cell.descriptionLabel.numberOfLines = 1
+        cell.descriptionLabel.minimumScaleFactor = 0.5
+        cell.descriptionLabel.adjustsFontSizeToFitWidth = true
+        
         cell.productTypeLabel.text = "Footwear"
+        cell.productTypeLabel.numberOfLines = 1
+        cell.productTypeLabel.adjustsFontSizeToFitWidth = true
+        cell.productTypeLabel.minimumScaleFactor = 0.5
+        
         cell.regionLabel.text = "NEW YORK, NY"
-        cell.itemsRemaining.text = "1236"
-        cell.itemsRemainingText.text = "backers"
-        cell.totalAmount.text = "$109,810 USD"
-        cell.plegdedFor.text = "pledged of $25,000"
-        cell.remainingHours.text = "54"
-        cell.hoursToGo.text = "hours to go"
+        cell.regionLabel.numberOfLines = 1
+        cell.regionLabel.adjustsFontSizeToFitWidth = true
+        cell.regionLabel.minimumScaleFactor = 0.5
+        
+        cell.itemsRemainingLabel.text = "1236"
+        cell.itemsRemainingLabel.numberOfLines = 1
+        cell.itemsRemainingLabel.adjustsFontSizeToFitWidth = true
+        cell.itemsRemainingLabel.minimumScaleFactor = 0.5
+        
+        cell.itemsRemainingTextLabel.text = "backers"
+        cell.itemsRemainingTextLabel.numberOfLines = 1
+        cell.itemsRemainingTextLabel.adjustsFontSizeToFitWidth = true
+        cell.itemsRemainingTextLabel.minimumScaleFactor = 0.5
+        
+        cell.totalAmountLabel.text = "$109,810 USD"
+        cell.totalAmountLabel.numberOfLines = 1
+        cell.totalAmountLabel.adjustsFontSizeToFitWidth = true
+        cell.totalAmountLabel.minimumScaleFactor = 0.5
+        
+        cell.plegdedForLabel.text = "pledged of $25,000"
+        cell.plegdedForLabel.numberOfLines = 1
+        cell.plegdedForLabel.adjustsFontSizeToFitWidth = true
+        cell.plegdedForLabel.minimumScaleFactor = 0.5
+        
+        cell.remainingHoursLabel.text = "54"
+        cell.remainingHoursLabel.numberOfLines = 1
+        cell.remainingHoursLabel.adjustsFontSizeToFitWidth = true
+        cell.remainingHoursLabel.minimumScaleFactor = 0.5
+        
+        cell.hoursToGoLabel.text = "hours to go"
+        cell.hoursToGoLabel.numberOfLines = 1
+        cell.hoursToGoLabel.adjustsFontSizeToFitWidth = true
+        cell.hoursToGoLabel.minimumScaleFactor = 0.5
+        
         return cell
     }
 
